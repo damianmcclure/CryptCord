32541fe7a7c8a75eebd932b43804c7d4da3b44a4
Damian McClure <damianmcclure2019@jamesrumsey.net>
2019-07-27 23:31:06 -0400

    Fixed for newer discord
    Credits to @atipls

--- Encrypt.plugin.js
+++ Encrypt.plugin.js
@@ -1,21 +1,37 @@
 //META{"name":"CryptCord"}*//
 //.markup is .da-markup now
-//Credits to: AtiPLS for ideas and etc
+//Credits to: AtiPLS for ideas, fixing, and etc.
 class CryptCord {
     constructor(){
         this.toggle = false; //ignore this
         this.key = "0123456789ABCDEF0123456789ABCDEF"; //Default key, u can change this in settings.
-
+        jQuery.expr[':'].regex = function (elem, index, match) {
+            var matchParams = match[3].split(','),
+                validLabels = /^(data|css):/,
+                attr = {
+                    method: matchParams[0].match(validLabels) ?
+                        matchParams[0].split(':')[0] : 'attr',
+                    property: matchParams.shift().replace(validLabels, '')
+                },
+                regexFlags = 'ig',
+                regex = new RegExp(matchParams.join('').replace(/^\s+|\s+$/g, ''), regexFlags);
+            return regex.test(jQuery(elem)[attr.method](attr.property));
+        }
         //load libs etc
         var libraryScript = document.createElement("script");
 		libraryScript.setAttribute("type", "text/javascript");
 		libraryScript.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/components/core-min.js");
         document.head.appendChild(libraryScript);
 
-	    libraryScript = document.createElement("script");
-		libraryScript.setAttribute("type", "text/javascript");
-		libraryScript.setAttribute("src", "https://rauenzi.github.io/BetterDiscordAddons/Plugins/PluginLibrary.js");
-		document.head.appendChild(libraryScript);
+        libraryScript = document.getElementById('ZLibraryScript');
+        if (!window.ZLibrary && !libraryScript) {
+            libraryScript = document.createElement('script');
+            libraryScript.setAttribute('type', 'text/javascript');
+            libraryScript.addEventListener("error", function () { if (typeof window.ZLibrary === "undefined") { window.BdApi.alert("Library Missing", `The library plugin needed for ${this.getName()} is missing and could not be loaded.<br /><br /><a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">Click here to download the library!</a>`); } }.bind(this));
+            libraryScript.setAttribute('src', 'https://rauenzi.github.io/BDPluginLibrary/release/ZLibrary.js');
+            libraryScript.setAttribute('id', 'ZLibraryScript');
+            document.head.appendChild(libraryScript);
+        }
         
         libraryScript = document.createElement("script");
 		libraryScript.setAttribute("type", "text/javascript");
@@ -44,7 +60,8 @@ class CryptCord {
 			console.log("Salt: "+encd.salt.toString());
 			console.log("IV..: "+encd.iv.toString());
 			return encd;
-        } catch(err) {
+        } catch (err) {
+            console.log(err);
             return err;
         }
     }
@@ -84,7 +101,7 @@ class CryptCord {
     }
 
     decryptAll(){
-        var data = PluginUtilities.loadData("Encrypt", "key");
+        var data = ZLibrary.PluginUtilities.loadData("Encrypt", "key");
         this.key = data.key;
         var self = this;
         $(".da-markup").each(function(){
@@ -107,8 +124,6 @@ class CryptCord {
     }
 
     start(){
-        PluginUtilities.checkForUpdate(this.getName(), this.getVersion(), "https://raw.githubusercontent.com/mcclureski/CryptCord/master/Encrypt.plugin.js");
-
         setTimeout(function(){console.log("[CryptCord] Init Etc")}, 10000);
 
         var self = this;
@@ -142,11 +157,11 @@ class CryptCord {
 
     saveSettings(){
         this.key = $("#zugg").val();
-        PluginUtilities.saveData("Encrypt", "key", {key: this.key});
+        ZLibrary.PluginUtilities.saveData("Encrypt", "key", {key: this.key});
     }
 
     getSettingsPanel(){
-        var data = PluginUtilities.loadData("Encrypt", "key");
+        var data = ZLibrary.PluginUtilities.loadData("Encrypt", "key");
         if(!data.key) { data.key = this.key; }
         return `<div style="color: white; padding: 20px;">
             Key: <input id="zugg" style="padding: 10px; width:70%;outline:none;color: white;border: none; border-radius: 5px; background-color: hsla(218,5%,47%,.3);" value=${data.key}><br><br>
@@ -172,35 +187,39 @@ class CryptCord {
 
     onSwitch(){
         var self = this;
-        if($(".name-3YKhmS").html()){
-			if($(".name-3YKhmS").html().startsWith("&#8203;") || $(".name-3YKhmS").html().startsWith("U2") || $(".name-3YKhmS").html().startsWith("\u200B")){
+        var name_selector = "div:regex(class, .*name-.*)"; 
+        var topic_selector = "div:regex(class, .*topic-.*)"; 
+        var members_selector = "div:regex(class, .*membersGroup-.*)"; 
+        var embed_selector = "div:regex(class, .*embedFieldName-.*)"; 
+        if ($(name_selector).html()){
+			if($(name_selector).html().startsWith("&#8203;") || $(name_selector).html().startsWith("U2") || $(name_selector).html().startsWith("\u200B")){
 				console.log("[CryptCord] Server Name Formatted");
-				var encrypted = $(".name-3YKhmS").html().replace("\u200B", "").replace("&#8203;", "");
+				var encrypted = $(name_selector).html().replace("\u200B", "").replace("&#8203;", "");
 				var decrypted = this.decrypt(encrypted);
-				if(decrypted == "" || decrypted == " " || decrypted == null || !decrypted || decrypted.length < 1){
-					$(".name-3YKhmS").html('<span style="color: #e21f1f;">'+encrypted+'</span>');
+                if(decrypted == "" || decrypted == " " || decrypted == null || !decrypted || decrypted.length < 1){
+                    $(name_selector).html('<span style="color: #e21f1f;">'+encrypted+'</span>');
 				} else if(decrypted.toString().toLowerCase().includes("error")) {
-					$(".name-3YKhmS").html('<span style="color: #e21f1f;">'+encrypted+'</span>');
-				} else {
-					$(".name-3YKhmS").html('<span style="color: #28e24e;">'+this.formatMsg(decrypted)+'</span>');
+					$(name_selector).html('<span style="color: #e21f1f;">'+encrypted+'</span>');
+				} else { 
+					$(name_selector).html('<span style="color: #28e24e;">'+this.formatMsg(decrypted)+'</span>');
 				}
 			}
         }
-        if($(".topic-2QX7LI").html()){
-			if($(".topic-2QX7LI").html().startsWith("&#8203;") || $(".topic-2QX7LI").html().startsWith("U2") || $(".topic-2QX7LI").html().startsWith("\u200B")){
+        if($(topic_selector).html()){
+			if($(topic_selector).html().startsWith("&#8203;") || $(topic_selector).html().startsWith("U2") || $(topic_selector).html().startsWith("\u200B")){
 				console.log("[CryptCord] Topics Formatted");
-				var encrypted = $(".topic-2QX7LI").html().replace("\u200B", "").replace("&#8203;", "");
+				var encrypted = $(topic_selector).html().replace("\u200B", "").replace("&#8203;", "");
 				var decrypted = this.decrypt(encrypted);
 				if(decrypted == "" || decrypted == " " || decrypted == null || !decrypted || decrypted.length < 1){
-					$(".topic-2QX7LI").html('<span style="color: #e21f1f;">'+encrypted+'</span>');
+					$(topic_selector).html('<span style="color: #e21f1f;">'+encrypted+'</span>');
 				} else if(decrypted.toString().toLowerCase().includes("error")) {
-					$(".topic-2QX7LI").html('<span style="color: #e21f1f;">'+encrypted+'</span>');
+					$(topic_selector).html('<span style="color: #e21f1f;">'+encrypted+'</span>');
 				} else {
-					$(".topic-2QX7LI").html('<span style="color: #28e24e;">'+this.formatMsg(decrypted)+'</span>');
+					$(topic_selector).html('<span style="color: #28e24e;">'+this.formatMsg(decrypted)+'</span>');
 				}
 			}
         }
-        $(".membersGroup-v9BXpm").each(function(){
+        $(members_selector).each(function(){
 			if($(this).html().startsWith("&#8203;") || $(this).html().startsWith("U2") || $(this).html().startsWith("\u200B")){
 				console.log("[CryptCord] One Role Formatted "+$(this).html());
 				var ii = $(this);
@@ -217,7 +236,7 @@ class CryptCord {
 				}
 			}
         });
-        $(".embedFieldName-NFrena").each(function(){
+        $(embed_selector).each(function(){
 			if($(this).html().startsWith("&#8203;") || $(this).html().startsWith("U2") || $(this).html().startsWith("\u200B")){
 				console.log("[CryptCord] Embed Titles Formatted "+$(this).html());
 				var ii = $(this);
