a9975a11ffb40ca74945b05993cd05416e61a427
Damian McClure <damianmcclure2019@jamesrumsey.net>
2019-10-09 19:09:57 -0400

    Update Encrypt.plugin.js
    Updated the BetterDiscord Settings Panel to offer stronger encryption key generation.

--- Encrypt.plugin.js
+++ Encrypt.plugin.js
@@ -2,8 +2,10 @@
 class CryptCord {
     constructor(){
         this.toggle = false;
-        this.key = "0123456789ABCDEF0123456789ABCDEF";
+        this.key = "0123456789ABCDEF0123456789ABCDEF"; // The DEFAULT Key. Please do not use this.
         var libraryScript;
+
+        this.encryption = "aes256";
 
         /* JQuery Library           */ libraryScript = document.createElement("script"); libraryScript.setAttribute("type", "text/javascript"); libraryScript.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/components/core-min.js"); document.head.appendChild(libraryScript);
         /* BetterDiscord Library    */ libraryScript = document.getElementById('ZLibraryScript'); if (!window.ZLibrary && !libraryScript) { libraryScript = document.createElement('script'); libraryScript.setAttribute('type', 'text/javascript'); libraryScript.addEventListener("error", function () { if (typeof window.ZLibrary === "undefined") { window.BdApi.alert("Library Missing", `The library plugin needed for ${this.getName()} is missing and could not be loaded.<br /><br /><a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">Click here to download the library!</a>`); } }.bind(this)); libraryScript.setAttribute('src', 'https://rauenzi.github.io/BDPluginLibrary/release/ZLibrary.js'); libraryScript.setAttribute('id', 'ZLibraryScript'); document.head.appendChild(libraryScript);}
@@ -117,10 +119,10 @@ class CryptCord {
         });
     }
 
-    randomKey(){
+    randomKey(num){
         let result = "";
         var possible = "0123456789ABCDEF0123456789ABCDEF";
-        for (var i = 0; i < possible.length; i++) {
+        for (var i = 0; i < num; i++) {
             result += possible.charAt(Math.floor(Math.random() * possible.length));
         }
         $("#cryptkey").val(result);
@@ -134,7 +136,23 @@ class CryptCord {
     getSettingsPanel(){
         var data = ZLibrary.PluginUtilities.loadData("Encrypt", "key");
         if(!data.key) { data.key = this.key; }
-        return `<div style="color: white; padding: 20px;"> Key: <input id="cryptkey" style="padding: 10px; width:70%;outline:none;color: white;border: none; border-radius: 5px; background-color: hsla(218,5%,47%,.3);" value=${data.key}><br><br> <button onclick="BdApi.getPlugin('${this.getName()}').saveSettings()" style="background: #7289da;color: #FFF;border-radius: 5px;">Save Settings</button>&nbsp;<button onclick="BdApi.getPlugin('${this.getName()}').randomKey()" style="background: #7289da;color: #FFF;border-radius: 5px;">Generate Key</button></div>`;
+        return `<div style="color: white; padding: 20px;">
+        Key: <input id="cryptkey" style="padding: 10px; width:70%;outline:none;color: white;border: none; border-radius: 5px; background-color: hsla(218,5%,47%,.3);" value=${data.key}><br><br> 
+        <table>
+            <tr>
+                <td><button onclick="BdApi.getPlugin('${this.getName()}').randomKey(32)" style="font-size: 12px !important;background: #e54e4e;color: #FFF;border-radius: 5px;">Generate 32 Bit Key</button></td>
+                <td><button onclick="BdApi.getPlugin('${this.getName()}').randomKey(64)" style="font-size: 12px !important;background: #e0cb43;color: #FFF;border-radius: 5px;">Generate 64 Bit Key</button></td>
+                <td><button onclick="BdApi.getPlugin('${this.getName()}').randomKey(128)" style="font-size: 12px !important;background: #47c134;color: #FFF;border-radius: 5px;">Generate 128 Bit Key</button></div></td>
+                <td><button onclick="BdApi.getPlugin('${this.getName()}').randomKey(128)" style="font-size: 12px !important;background: #b643e0;color: #FFF;border-radius: 5px;">Generate 256 Bit Key</button></div></td>
+            </tr>
+            <tr>
+                <td style="text-align: center;">Least Secure</td>
+                <td style="text-align: center;">Kinda Secure</td>
+                <td style="text-align: center;">Very Secure</td>
+                <td style="text-align: center;">Expirimental!</td>
+            </tr>
+        </table>
+       <div style="float: right;"><button onclick="BdApi.getPlugin('${this.getName()}').saveSettings()" style="background: #7289da;color: #FFF;border-radius: 5px;">Save Settings</button></div>`;
     }
     
     start(){
@@ -205,104 +223,3 @@ class CryptCord {
         this.decryptAll();
     }
 }
-!"; }
-    getVersion     () { return "0.1.0"; }
-    getAuthor      () { return "Mcclures"; }
-    
-    createButton(){
-        $('<div class="buttons-205you" style="padding-right: 0px !important;">'+this.buttonHTML+'</div>').insertBefore(".da-buttons");
-    }
-
-    changeButton(){
-        $("#encryption_button").html(this.buttonHTML);
-    }
-
-    encrypt(text){
-        try {
-		    var encd = CryptoJS.AES.encrypt(text, this.key);
-		    console.log("Salt: "+encd.salt.toString());
-		    console.log("IV..: "+encd.iv.toString());
-		    return encd;
-        } catch (err) {
-            console.log(err);
-            return err;
-        }
-    }
-
-    decrypt(text){
-        try {
-            return CryptoJS.AES.decrypt(text, this.key).toString(CryptoJS.enc.Utf8);
-        } catch(err) {
-            return err;
-        }
-    }
-
-    formatMsg(msg){
-		if(!msg){
-            return "Error";
-        } else {
-            var bold = /\*\*([^*]+)\*\*/g;
-            var italic = /\*([^*]+)\*/g;
-            var underline = /\_([^*]+)\_/g;
-            var strike = /\~\~([^*]+)\~\~/g;
-            var url = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-            var inline = /\`([^*]+)\`/g;
-            var codeblocksingle = /\`\`\`([^*]+)\`\`\`/g;
-            var codeblockmulti = /\`\`\`(\w+)\n((?:(?!\`\`\`)[\s\S])*)\`\`\`/g;
-
-            msg = msg.replace(bold, "<b>$1</b>");
-            msg = msg.replace(italic, "<i>$1</i>");
-            msg = msg.replace(underline, "<U>$1</U>");
-            msg = msg.replace(strike, "<s>$1</s>");
-            msg = msg.replace(codeblockmulti, `<pre><code class="scrollbarGhost-2F9Zj2 scrollbar-3dvm_9 hljs $1" style="position: relative;">$2</code></pre>`);
-            msg = msg.replace(codeblocksingle, `<pre><code class="scrollbarGhost-2F9Zj2 scrollbar-3dvm_9 hljs" style="position: relative;">$1</code></pre>`);
-            msg = msg.replace(inline, `<code class="inline">$1</code>`);
-            msg = msg.replace(url, "<a href='$1'>$1</a>");
-            return msg;
-        }
-    }
-
-    decryptAll(){
-        var data = ZLibrary.PluginUtilities.loadData("Encrypt", "key");
-        if(!data.key){
-            data.key = this.key;
-        }
-        this.key = data.key;
-        var self = this;
-        $(".da-markup").each(function(){
-            var i = $(this).html();
-            if(i.startsWith('\u200B')){
-                $(this).html(function (_, html) {
-                    console.log(i);
-                    var encrypted = i.replace('\u200B', "");
-                    var decrypted = self.decrypt(encrypted);
-                    if(decrypted == "" || decrypted == " " || decrypted == null || !decrypted || decrypted.length < 1){
-                        return html.replace(i, '<span style="color: #e21f1f;">'+encrypted+'</span>');
-                    } else if(decrypted.toString().toLowerCase().includes("error")) {
-                        return html.replace(i, '<span style="color: #e21f1f;">'+decrypted+'</span>');
-                    } else {
-                        return html.replace(i, '<span style="color: #28e24e;">'+self.formatMsg(decrypted)+'</span>');
-                    }
-                });
-            }
-        });
-    }
-
-    randomKey(){
-        let result = "";
-        var possible = "0123456789ABCDEF0123456789ABCDEF";
-        for (var i = 0; i < possible.length; i++) {
-            result += possible.charAt(Math.floor(Math.random() * possible.length));
-        }
-        $("#cryptkey").val(result);
-    }
-
-    saveSettings(){
-        this.key = $("#cryptkey").val();
-        ZLibrary.PluginUtilities.saveData("Encrypt", "key", {key: this.key});
-    }
-
-    getSettingsPanel(){
-        var data = ZLibrary.PluginUtilities.loadData("Encrypt", "key");
-        if(!data.key) { data.key = this.key; }
-        return `<div style="color: white; padding: 20px;"> Key: <input id="cryptkey" style="padding: 10px; width:70%;
