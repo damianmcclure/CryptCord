6f4f99b06afe993cd9111b57fe4bf7d30bd5bf75
Damian McClure <damianmcclure2019@jamesrumsey.net>
2019-10-09 01:13:43 -0400

    Update Encrypt.plugin.js

--- Encrypt.plugin.js
+++ Encrypt.plugin.js
@@ -205,3 +205,104 @@ class CryptCord {
         this.decryptAll();
     }
 }
+!"; }
+    getVersion     () { return "0.1.0"; }
+    getAuthor      () { return "Mcclures"; }
+    
+    createButton(){
+        $('<div class="buttons-205you" style="padding-right: 0px !important;">'+this.buttonHTML+'</div>').insertBefore(".da-buttons");
+    }
+
+    changeButton(){
+        $("#encryption_button").html(this.buttonHTML);
+    }
+
+    encrypt(text){
+        try {
+		    var encd = CryptoJS.AES.encrypt(text, this.key);
+		    console.log("Salt: "+encd.salt.toString());
+		    console.log("IV..: "+encd.iv.toString());
+		    return encd;
+        } catch (err) {
+            console.log(err);
+            return err;
+        }
+    }
+
+    decrypt(text){
+        try {
+            return CryptoJS.AES.decrypt(text, this.key).toString(CryptoJS.enc.Utf8);
+        } catch(err) {
+            return err;
+        }
+    }
+
+    formatMsg(msg){
+		if(!msg){
+            return "Error";
+        } else {
+            var bold = /\*\*([^*]+)\*\*/g;
+            var italic = /\*([^*]+)\*/g;
+            var underline = /\_([^*]+)\_/g;
+            var strike = /\~\~([^*]+)\~\~/g;
+            var url = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+            var inline = /\`([^*]+)\`/g;
+            var codeblocksingle = /\`\`\`([^*]+)\`\`\`/g;
+            var codeblockmulti = /\`\`\`(\w+)\n((?:(?!\`\`\`)[\s\S])*)\`\`\`/g;
+
+            msg = msg.replace(bold, "<b>$1</b>");
+            msg = msg.replace(italic, "<i>$1</i>");
+            msg = msg.replace(underline, "<U>$1</U>");
+            msg = msg.replace(strike, "<s>$1</s>");
+            msg = msg.replace(codeblockmulti, `<pre><code class="scrollbarGhost-2F9Zj2 scrollbar-3dvm_9 hljs $1" style="position: relative;">$2</code></pre>`);
+            msg = msg.replace(codeblocksingle, `<pre><code class="scrollbarGhost-2F9Zj2 scrollbar-3dvm_9 hljs" style="position: relative;">$1</code></pre>`);
+            msg = msg.replace(inline, `<code class="inline">$1</code>`);
+            msg = msg.replace(url, "<a href='$1'>$1</a>");
+            return msg;
+        }
+    }
+
+    decryptAll(){
+        var data = ZLibrary.PluginUtilities.loadData("Encrypt", "key");
+        if(!data.key){
+            data.key = this.key;
+        }
+        this.key = data.key;
+        var self = this;
+        $(".da-markup").each(function(){
+            var i = $(this).html();
+            if(i.startsWith('\u200B')){
+                $(this).html(function (_, html) {
+                    console.log(i);
+                    var encrypted = i.replace('\u200B', "");
+                    var decrypted = self.decrypt(encrypted);
+                    if(decrypted == "" || decrypted == " " || decrypted == null || !decrypted || decrypted.length < 1){
+                        return html.replace(i, '<span style="color: #e21f1f;">'+encrypted+'</span>');
+                    } else if(decrypted.toString().toLowerCase().includes("error")) {
+                        return html.replace(i, '<span style="color: #e21f1f;">'+decrypted+'</span>');
+                    } else {
+                        return html.replace(i, '<span style="color: #28e24e;">'+self.formatMsg(decrypted)+'</span>');
+                    }
+                });
+            }
+        });
+    }
+
+    randomKey(){
+        let result = "";
+        var possible = "0123456789ABCDEF0123456789ABCDEF";
+        for (var i = 0; i < possible.length; i++) {
+            result += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        $("#cryptkey").val(result);
+    }
+
+    saveSettings(){
+        this.key = $("#cryptkey").val();
+        ZLibrary.PluginUtilities.saveData("Encrypt", "key", {key: this.key});
+    }
+
+    getSettingsPanel(){
+        var data = ZLibrary.PluginUtilities.loadData("Encrypt", "key");
+        if(!data.key) { data.key = this.key; }
+        return `<div style="color: white; padding: 20px;"> Key: <input id="cryptkey" style="padding: 10px; width:70%;
